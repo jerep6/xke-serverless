01915f61576363e3be6096b86eae0fb93c68c2e2
Jérémy Pinsolle <jpinsolle@xebia.fr>
2018-02-21 15:28:18 +0100

    Hands-on

--- nodejs-express/app/controllers/BookController.ts
+++ nodejs-express/app/controllers/BookController.ts
@@ -2,7 +2,6 @@ import * as express from 'express';
 import {Request, Response, NextFunction} from 'express';
 
 import * as repo from '../repositories/BookRepository'
-import * as repoDynamo from '../repositories/BookRepositoryDynamo'
 import logger from '../utils/logger.utils';
 import { Book } from '../../typings/Book';
 
@@ -15,7 +14,7 @@ async function list(req: Request, res: Response, next: NextFunction) {
   logger.debug('list books');
 
   try {
-    const books: Book[] = await repoDynamo.list();
+    const books: Book[] = await repo.list();
     res
       .json(books);
   }
@@ -29,7 +28,7 @@ async function get(req: Request, res: Response, next: NextFunction) {
   logger.debug(`get book ${bookId}`);
 
   try {
-    const book: Book = await repoDynamo.get(bookId);
+    const book: Book = await repo.get(bookId);
     res
       .status(book ? 200: 404)
       .json(book)
