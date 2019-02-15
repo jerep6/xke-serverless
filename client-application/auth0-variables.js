aef5ba2c733bffac414bff3f8489da217aaf0de3
Jérémy Pinsolle <jpinsolle@xebia.fr>
2019-02-15 18:06:21 +0100

    Docker and lambda

--- client-application/auth0-variables.js
+++ client-application/auth0-variables.js
@@ -1,4 +1,4 @@
-var AUTH0_CLIENT_ID = 'sN0wrYcd3OZzvpvi8aP0uHYyZ4739yIs';
+var AUTH0_CLIENT_ID = 'FUan2mc4ujaFSjeZhdGxLGOxUfXUnDos';
 var AUTH0_DOMAIN = 'xebia-xke.eu.auth0.com';
-var AUTH0_AUDIENCE = 'myapi1';
+var AUTH0_AUDIENCE = 'api2';
 var AUTH0_CALLBACK_URL = location.href;
--- docker-and-lambda/app/LambdaHandler.ts
+++ docker-and-lambda/app/LambdaHandler.ts
@@ -1,6 +1,6 @@
 import { APIGatewayEvent, Context as LambdaContext, ProxyCallback } from 'aws-lambda';
 import * as serverlessHttp from 'serverless-http';
-import app from '../index';
+import app from '.';
 
 export async function handler(event: APIGatewayEvent, context: LambdaContext, callback: ProxyCallback) {
   return serverlessHttp(app)(event, context);
